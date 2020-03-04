8368b87603c1b66ede8bb6ca3f6f621936aaf15f
YanoRyo <yanoryo7032@gmail.com>
2020-03-04 16:13:43 -0500

    Initial Commit

---
+++ Oreno1App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Oreno1App
+//
+//  Created by 矢野涼 on 2020-03-04.
+//  Copyright © 2020 Ryo Yano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
